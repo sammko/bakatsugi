5295fec8400dfac306fe31dfa61df649f60c321d
Samuel Čavoj <samuel@cavoj.net>
2022-02-01 11:25:54 +0000

    stage2: Implement library function patch PoC

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
 #![feature(unix_socket_abstract)]
+#![feature(c_size_t)]
 
 use core::slice;
-use std::{arch::asm, os::unix::net::{UnixStream, SocketAddr}, io::Write};
+use std::{
+    arch::asm,
+    env, fs,
+    io::Write,
+    os::{
+        raw::{c_size_t, c_ssize_t},
+        unix::net::{SocketAddr, UnixStream},
+    },
+    ptr::slice_from_raw_parts,
+};
 
 use anyhow::{bail, Context, Result};
 use ctor::ctor;
-use nix::unistd::getpid;
+use goblin::{elf::Elf, elf64::reloc::R_X86_64_GLOB_DAT};
+use nix::{
+    libc::{self, c_int, c_void, getauxval, AT_PHDR},
+    sys::mman::{mprotect, ProtFlags},
+    unistd::getpid,
+};
 use proc_maps::MapRange;
+use rand::{prelude::SliceRandom, thread_rng};
 
 include!(concat!(env!("OUT_DIR"), "/payload_constants.rs"));
 const MAGIC: u64 = 0x68637450616b6142;
@@ -67,6 +83,78 @@ fn get_payload_data(self_vmaddr: u64) -> Result<PayloadData> {
     }
 }
 
+fn patch_reloc(name: &str, fake_fun: usize) -> Result<()> {
+    // hmm maybe we should be doing this from the outside instead
+    let data = fs::read("/proc/self/exe")?;
+    let elf = Elf::parse(&data)?;
+
+    // Find relocation for given symbol
+    let symbol_reloc = elf
+        .dynrelas
+        .iter()
+        .find(|r| {
+            elf.dynsyms
+                .get(r.r_sym)
+                .and_then(|s| elf.dynstrtab.get_at(s.st_name))
+                == Some(name)
+        })
+        .context(format!("Could not find reloc for {}", name))?;
+
+    if symbol_reloc.r_type != R_X86_64_GLOB_DAT {
+        bail!("Unsupported relocation {}", symbol_reloc.r_type)
+    }
+
+    let offset = symbol_reloc.r_offset;
+
+    // Find PHDR containing the relocation's target
+    let phdr = elf
+        .program_headers
+        .iter()
+        .find(|&phdr| phdr.vm_range().contains(&(offset as usize)))
+        .context("Could not find phdr containing reloc offset")?;
+
+    // TODO is this correct?
+    let phdr_vma = unsafe { getauxval(AT_PHDR) };
+    let x = phdr_vma - elf.header.e_phoff;
+    let got_entry = x + offset;
+
+    eprintln!("phdr is: {:?}", phdr);
+    eprintln!("base: {:x}", x);
+    eprintln!("GOT entry at vma {:x}", x + offset);
+    eprintln!("fake at {:x}", fake_fun);
+
+    unsafe {
+        // bypass RELRO
+        mprotect(
+            (got_entry & 0xfffffffffffff000) as *mut c_void,
+            4096,
+            ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+        )?;
+        *((x + offset) as *mut usize) = fake_fun;
+    }
+
+    Ok(())
+}
+
+pub unsafe extern "C" fn fakewrite(
+    fildes: c_int,
+    buf: *const c_void,
+    nbyte: c_size_t,
+) -> c_ssize_t {
+    if nbyte > 0 {
+        let inp = &*slice_from_raw_parts(buf as *const u8, nbyte);
+        let mut tmp = inp.to_owned();
+        if inp[nbyte - 1] == b'\n' {
+            tmp[..nbyte - 1].shuffle(&mut thread_rng());
+        } else {
+            tmp.shuffle(&mut thread_rng());
+        }
+        libc::write(fildes, tmp.as_ptr() as *const c_void, nbyte)
+    } else {
+        0
+    }
+}
+
 fn init() -> Result<()> {
     let pid = getpid();
     let maps = proc_maps::get_process_maps(pid.as_raw())?;
@@ -78,6 +166,8 @@ fn init() -> Result<()> {
     let addr = SocketAddr::from_abstract_namespace(&data.cookie)?;
     let mut sock = UnixStream::connect_addr(&addr)?;
     sock.write(b"Hello")?;
+
+    patch_reloc("write", fakewrite as usize)?;
 
     Ok(())
 }
