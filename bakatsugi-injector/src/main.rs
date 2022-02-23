51ab4cc9e0c60cf0f8a204e7fcd08173223efd95
Samuel Čavoj <samuel@cavoj.net>
2022-02-23 23:51:10 +0100

    Move `fn generate_payload` to `bakatsugi-payload` package

--- bakatsugi-injector/src/main.rs
+++ bakatsugi-injector/src/main.rs
@@ -3,14 +3,12 @@
 #![feature(unix_socket_abstract)]
 #![feature(unix_socket_ancillary_data)]
 
-use bakatsugi_payload::PAYLOAD_ELF;
 use std::{
     ffi::{c_void, CStr, OsString},
     fs::{self, File},
     io::{IoSlice, Read, Write},
     mem,
     net::Shutdown,
-    ops::Range,
     os::unix::{
         net::SocketAddr,
         net::{SocketAncillary, UnixListener, UnixStream},
@@ -20,10 +18,8 @@ use std::{
 };
 
 use anyhow::{bail, Context, Result};
-use goblin::{
-    elf::{Elf, Sym},
-    Object,
-};
+use bakatsugi_payload::generate_payload;
+use goblin::Object;
 use nix::{
     libc::{
         SYS_mmap, MAP_ANONYMOUS, MAP_PRIVATE, PROT_EXEC, PROT_READ, PROT_WRITE, PTRACE_EVENT_STOP,
@@ -109,54 +105,6 @@ fn create_bakatsugi_memfd() -> Result<RawFd> {
     file.write_all(STAGE2_ELF)?;
     mem::forget(file);
     Ok(memfd)
-}
-
-fn generate_payload(self_vmaddr: u64, dlopen_vmaddr: u64, cookie: &[u8; 16]) -> Result<Vec<u8>> {
-    let elf = Elf::parse(PAYLOAD_ELF)?;
-
-    if elf.program_headers.len() != 1 {
-        bail!("elf must contain exactly one phdr")
-    }
-    let phdr = elf.program_headers.get(0).unwrap();
-
-    let mut sym_self = None;
-    let mut sym_dlopen = None;
-    let mut sym_cookie = None;
-
-    for sym in elf.syms.iter() {
-        match elf.strtab.get_at(sym.st_name) {
-            Some("self") => sym_self = Some(sym),
-            Some("dlopen") => sym_dlopen = Some(sym),
-            Some("cookie") => sym_cookie = Some(sym),
-            Some(_) | None => {}
-        }
-    }
-
-    fn r(sym: &Sym, size: usize) -> Range<usize> {
-        Range {
-            start: sym.st_value as usize,
-            end: sym.st_value as usize + size,
-        }
-    }
-
-    let range_self = r(&sym_self.context("Symbol self missing")?, 8);
-    let range_dlopen = r(&sym_dlopen.context("Symbol dlopen missing")?, 8);
-    let range_cookie = r(&sym_cookie.context("Symbol cookie missing")?, 16);
-
-    let actual_end = [&range_self, &range_dlopen, &range_cookie]
-        .iter()
-        .map(|r| r.end)
-        .max()
-        .unwrap();
-
-    let mut payload = PAYLOAD_ELF[phdr.file_range()].to_owned();
-    payload.resize(actual_end, 0);
-
-    payload[range_self].copy_from_slice(&self_vmaddr.to_le_bytes());
-    payload[range_dlopen].copy_from_slice(&dlopen_vmaddr.to_le_bytes());
-    payload[range_cookie].copy_from_slice(cookie);
-
-    Ok(payload)
 }
 
 fn bind_listener(cookie: &[u8; 16]) -> Result<UnixListener> {
